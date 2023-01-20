0a1864f4a95783496c1533f8b7d3ca6d71ca222a
Momfus <momfus@outlook.com>
2023-01-20 03:29:41 -0300

    Show game list

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/enviroment';
 import { Game } from '../interfaces/interfaces';
 
@@ -8,13 +10,30 @@ import { Game } from '../interfaces/interfaces';
 })
 export class GameService {
 
+  private games: Game[] = [];
+
   constructor(
     private http: HttpClient
   ) { }
 
   getNominated() {
 
-    return this.http.get<Game[]>(`${ environment.url }/api/goty`);
+    if( this.games.length > 0 ) {
+
+      // Cache loaded
+      return of(this.games)
+
+    } else {
+
+      return this.http.get<Game[]>(`${ environment.url }/api/goty`)
+        .pipe(
+          tap(
+            games => this.games = games
+          )
+        );
+
+    }
+
 
   }
 }
