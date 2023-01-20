1b6565977aefade2d69859cf72d391566569a4ed
Momfus <momfus@outlook.com>
2023-01-20 05:06:43 -0300

    Change production values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+import { MY_FIRESTORE_KEY } from './myKeyFiles';
 
 
 export const environment = {
   production: true,
-  url: 'https://us-central1-firestore-graph-goty.cloudfunctions.net'
+  url: 'https://us-central1-firestore-graph-goty.cloudfunctions.net',
+  firebase: MY_FIRESTORE_KEY // This key is not upload, for the configuration see: https://github.com/angular/angularfire/blob/master/docs/install-and-setup.md
 };
